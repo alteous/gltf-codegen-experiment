aa53c3d5c694320aca3d0a488b387b420a71c257
David Harvey-Macaulay <alteous@outlook.com>
2019-07-17 19:57:52 +0100

    Add initial boilerplate code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+type Result<T> = std::result::Result<T, std::boxed::Box<dyn std::error::Error>>;
+type UnitResult = Result<()>;
+
+fn run() -> UnitResult {
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    run().expect("runtime error");
 }
